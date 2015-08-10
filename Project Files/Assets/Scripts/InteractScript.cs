6b9277de8fe452c5c370d42509978f936bea463a
John Dowling <j.j.dowling@hotmail.com>
2015-08-10 19:20:58 -0400

    Project Files Added

---
+++ Project Files/Assets/Scripts/InteractScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractScript : MonoBehaviour {
+
+	public float interactDistance = 5f;
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			Ray ray = new Ray(transform.position, transform.forward);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit, interactDistance))
+			{
+				if(hit.collider.CompareTag("Door"))
+				{
+					hit.collider.transform.parent.GetComponent<DoorScript>().ChangeDoorState();
+				}
+			}
+			
+		}
+	}
+}
---
+++ Project Files/Assets/flashlightFlicker.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightFlicker : MonoBehaviour {
+	
+	public float minTime = 0.1f;
+	public float maxTime = 0.6f;
+	
+	private float timer;
+	private Light l;
+
+	bool flicker = false;
+	
+	void Start () {
+		l = GetComponent<Light>();
+		timer = Random.Range(minTime, maxTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("fTrigger")) 
+		{
+
+			flicker = true;
+			
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag ("fTrigger")) {
+			flicker = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag ("fTrigger")) {
+			flicker = false;
+		}
+		}
+	
+ 	void Update()
+	{
+		if (flicker) {
+
+
+		timer -= Time.deltaTime;
+		if(timer <= 0)
+		{
+			l.enabled = !l.enabled;
+			timer = Random.Range(minTime, maxTime);
+		}
+		}
+	}
+}
